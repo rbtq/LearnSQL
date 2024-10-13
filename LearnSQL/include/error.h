dbd9b8bbd5f570271294fac100ea932225028ba8
rbtq <54366016+rbtq@users.noreply.github.com>
2024-10-13 14:36:27 +0100

    working on sql error func

--- LearnSQL/include/error.h
+++ LearnSQL/include/error.h
@@ -2,5 +2,9 @@
 #include "sqlIncludes.h"
 
 void showSQLError(unsigned int handleType, const SQLHANDLE& handle) { //!<prints the error
+	SQLWCHAR sqlState[1024];
+	SQLWCHAR message[1024];
+	if (SQL_SUCCESS == SQLGetDiagRec(handleType, handle, 1, sqlState, NULL, message, 1024, NULL)) {
 
+	}
 }
